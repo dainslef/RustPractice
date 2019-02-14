edbde46a0357a1059757884cfa071d19b50b1ab1
dainslef <dainslef@outlook.com>
2019-02-14 20:21:13 +0800

    Add the solution for the "Word Ladder II" problem.

--- src/leetcode/q126_find_ladders.rs
+++ src/leetcode/q126_find_ladders.rs
@@ -35,19 +35,79 @@
  */
 
 fn find_ladders(begin_word: String, end_word: String, word_list: Vec<String>) -> Vec<Vec<String>> {
-  let temp = vec![vec![]];
+  let mut temp: Vec<Vec<String>> = Vec::new();
+  let mut min_size = 0;
 
-  let get_exist_indexes = |vec: &Vec<String>| {
-
-  };
-
-  if word_list.contains(&end_word) {
-    for i in 0..end_word.len() {
-
+  fn dfs(
+    current: &String,
+    input_list: Vec<String>,
+    mut output_list: Vec<String>,
+    end_word: &String,
+    temp: &mut Vec<Vec<String>>,
+    min_size: &mut usize,
+  ) {
+    if check_word(&current, end_word) {
+      output_list.push(end_word.clone());
+      let size = output_list.len();
+      if *min_size == 0 || size < *min_size {
+        *min_size = size;
+      }
+      temp.push(output_list);
+      return;
+    }
+    for i in 0..input_list.len() {
+      let next = &input_list[i];
+      if check_word(&current, &next) {
+        let (mut new_output_list, mut new_input_list) = (output_list.clone(), input_list.clone());
+        new_output_list.push(next.clone());
+        new_input_list.remove(i);
+        dfs(
+          next,
+          new_input_list,
+          new_output_list,
+          end_word,
+          temp,
+          min_size,
+        );
+      }
     }
   }
 
+  if word_list.contains(&end_word) {
+    dfs(
+      &begin_word,
+      word_list,
+      vec![begin_word.clone()],
+      &end_word,
+      &mut temp,
+      &mut min_size,
+    );
+  }
+
+  dbg!((min_size, &temp));
+
   temp
+    .into_iter()
+    .filter(|v| v.last().map(|s| s == &end_word).unwrap_or(false))
+    .filter(|v| v.len() == min_size)
+    .collect::<Vec<Vec<String>>>()
+}
+
+fn check_word(old_word: &String, new_word: &String) -> bool {
+  let mut count = 0;
+  let (old_chars, new_chars) = (
+    old_word.chars().collect::<Vec<char>>(),
+    new_word.chars().collect::<Vec<char>>(),
+  );
+  for i in 0..old_chars.len() {
+    if old_chars[i] != new_chars[i] {
+      count += 1;
+      if count > 1 {
+        return false;
+      }
+    }
+  }
+  count == 1
 }
 
 #[test]
@@ -59,15 +119,72 @@ fn test_find_ladders() {
       .collect::<Vec<String>>()
   }
 
-  assert_eq!(
+  fn is_same(input1: Vec<Vec<String>>, input2: Vec<Vec<String>>) -> bool {
+    use std::collections::HashSet;
+    fn vec_to_set(input: Vec<Vec<String>>) -> HashSet<Vec<String>> {
+      let mut set = HashSet::new();
+      for i in input {
+        set.insert(i);
+      }
+      set
+    }
+    vec_to_set(input1) == vec_to_set(input2)
+  }
+
+  assert!(is_same(
     find_ladders(
       "hit".to_string(),
       "cog".to_string(),
       vec_string(&["hot", "dot", "dog", "lot", "log", "cog"])
     ),
     vec![
-      vec_string(&["hit", "hot", "dot", "dog", "cog"]),
-      vec_string(&["hit", "hot", "lot", "log", "cog"])
+      vec_string(&["hit", "hot", "lot", "log", "cog"]),
+      vec_string(&["hit", "hot", "dot", "dog", "cog"])
     ]
+  ));
+
+  assert_eq!(
+    find_ladders(
+      "hot".to_string(),
+      "dog".to_string(),
+      vec_string(&["hot", "dog", "dot"])
+    ),
+    vec![vec_string(&["hot", "dot", "dog"])]
+  );
+
+  assert_eq!(
+    find_ladders(
+      "a".to_string(),
+      "c".to_string(),
+      vec_string(&["a", "b", "c"])
+    ),
+    vec![vec_string(&["a", "c"])]
+  );
+
+  let v: Vec<Vec<String>> = vec![];
+  assert_eq!(
+    find_ladders(
+      "hit".to_string(),
+      "cog".to_string(),
+      vec_string(&["hot", "dot", "dog", "lot", "log"])
+    ),
+    v
+  );
+
+  println!(
+    "{:?}",
+    find_ladders(
+      "qa".to_string(),
+      "sq".to_string(),
+      vec_string(&[
+        "si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av",
+        "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr",
+        "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as",
+        "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz",
+        "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st",
+        "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr",
+        "pa", "he", "lr", "sq", "ye"
+      ])
+    )
   );
 }
