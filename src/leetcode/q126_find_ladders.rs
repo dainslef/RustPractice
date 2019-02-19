ed3ff55f96b37a9db6f2225b9c127cf56eba9e18
dainslef <dainslef@outlook.com>
2019-02-19 22:59:12 +0800

    Add a new solution for the "Word Ladder II" problem.

--- src/leetcode/q126_find_ladders.rs
+++ src/leetcode/q126_find_ladders.rs
@@ -1,4 +1,4 @@
-/**
+/* *
  * Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
  *
  * Only one letter can be changed at a time
@@ -34,9 +34,30 @@
  * Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
  */
 
-fn find_ladders(begin_word: String, end_word: String, word_list: Vec<String>) -> Vec<Vec<String>> {
+fn find_ladders_dfs(
+  begin_word: String,
+  end_word: String,
+  word_list: Vec<String>,
+) -> Vec<Vec<String>> {
   let mut temp: Vec<Vec<String>> = Vec::new();
   let mut min_size = 0;
+
+  fn check_word(old_word: &String, new_word: &String) -> bool {
+    let mut count = 0;
+    let (old_chars, new_chars) = (
+      old_word.chars().collect::<Vec<char>>(),
+      new_word.chars().collect::<Vec<char>>(),
+    );
+    for i in 0..old_chars.len() {
+      if old_chars[i] != new_chars[i] {
+        count += 1;
+        if count > 1 {
+          return false;
+        }
+      }
+    }
+    count == 1
+  }
 
   fn dfs(
     current: &String,
@@ -88,35 +109,67 @@ fn find_ladders(begin_word: String, end_word: String, word_list: Vec<String>) ->
 
   temp
     .into_iter()
-    .filter(|v| v.last().map(|s| s == &end_word).unwrap_or(false))
-    .filter(|v| v.len() == min_size)
-    .collect::<Vec<Vec<String>>>()
+    .filter(|v| v.len() == min_size && v.last().map(|s| s == &end_word).unwrap_or(false))
+    .collect()
 }
 
-fn check_word(old_word: &String, new_word: &String) -> bool {
-  let mut count = 0;
-  let (old_chars, new_chars) = (
-    old_word.chars().collect::<Vec<char>>(),
-    new_word.chars().collect::<Vec<char>>(),
-  );
-  for i in 0..old_chars.len() {
-    if old_chars[i] != new_chars[i] {
-      count += 1;
-      if count > 1 {
-        return false;
+fn find_ladders(begin_word: String, end_word: String, word_list: Vec<String>) -> Vec<Vec<String>> {
+  use std::collections::{HashSet, VecDeque};
+  use std::iter::FromIterator;
+
+  let mut word_set: HashSet<String> = HashSet::from_iter(word_list);
+  let mut paths: VecDeque<Vec<String>> = VecDeque::new();
+  let mut result: Vec<Vec<String>> = vec![];
+  let mut currents: HashSet<String> = HashSet::new();
+
+  paths.push_back(vec![begin_word]);
+
+  if word_set.contains(&end_word) {
+    let (mut level, mut min_level) = (1, word_set.len());
+    while let Some(path) = paths.pop_front() {
+      if path.len() > level {
+        for word in &currents {
+          word_set.remove(word);
+        }
+        currents.clear();
+        level = path.len();
+        if level > min_level {
+          break;
+        }
+      }
+      if let Some(current) = path.last() {
+        let bytes: Vec<u8> = current.bytes().collect();
+        for i in 0..bytes.len() {
+          // 'a' .. 'z' ASCII number
+          for u in 97_u8..=122_u8 {
+            let mut bytes = bytes.clone();
+            bytes[i] = u;
+            if let Ok(next) = String::from_utf8(bytes) {
+              if word_set.contains(&next) {
+                currents.insert(next.clone());
+                let mut new_path = path.clone();
+                new_path.push(next.clone());
+                if next == end_word {
+                  min_level = new_path.len();
+                  result.push(new_path);
+                } else {
+                  paths.push_back(new_path);
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
-  count == 1
+
+  result
 }
 
 #[test]
 fn test_find_ladders() {
   fn vec_string(str_array: &[&str]) -> Vec<String> {
-    str_array
-      .iter()
-      .map(|v| v.to_string())
-      .collect::<Vec<String>>()
+    str_array.iter().map(|v| v.to_string()).collect()
   }
 
   fn is_same(input1: Vec<Vec<String>>, input2: Vec<Vec<String>>) -> bool {
@@ -128,8 +181,11 @@ fn test_find_ladders() {
       }
       set
     }
+    dbg!((&input1, &input2));
     vec_to_set(input1) == vec_to_set(input2)
   }
+
+  let empty: Vec<Vec<String>> = vec![];
 
   assert!(is_same(
     find_ladders(
@@ -154,6 +210,15 @@ fn test_find_ladders() {
 
   assert_eq!(
     find_ladders(
+      "hot".to_string(),
+      "dog".to_string(),
+      vec_string(&["hot", "dog"])
+    ),
+    empty
+  );
+
+  assert_eq!(
+    find_ladders(
       "a".to_string(),
       "c".to_string(),
       vec_string(&["a", "b", "c"])
@@ -161,14 +226,13 @@ fn test_find_ladders() {
     vec![vec_string(&["a", "c"])]
   );
 
-  let v: Vec<Vec<String>> = vec![];
   assert_eq!(
     find_ladders(
       "hit".to_string(),
       "cog".to_string(),
       vec_string(&["hot", "dot", "dog", "lot", "log"])
     ),
-    v
+    empty
   );
 
   println!(
@@ -187,4 +251,61 @@ fn test_find_ladders() {
       ])
     )
   );
+
+  println!(
+    "{:?}",
+    find_ladders(
+      "cet".to_string(),
+      "ism".to_string(),
+      vec_string(&[
+        "kid", "tag", "pup", "ail", "tun", "woo", "erg", "luz", "brr", "gay", "sip", "kay", "per",
+        "val", "mes", "ohs", "now", "boa", "cet", "pal", "bar", "die", "war", "hay", "eco", "pub",
+        "lob", "rue", "fry", "lit", "rex", "jan", "cot", "bid", "ali", "pay", "col", "gum", "ger",
+        "row", "won", "dan", "rum", "fad", "tut", "sag", "yip", "sui", "ark", "has", "zip", "fez",
+        "own", "ump", "dis", "ads", "max", "jaw", "out", "btu", "ana", "gap", "cry", "led", "abe",
+        "box", "ore", "pig", "fie", "toy", "fat", "cal", "lie", "noh", "sew", "ono", "tam", "flu",
+        "mgm", "ply", "awe", "pry", "tit", "tie", "yet", "too", "tax", "jim", "san", "pan", "map",
+        "ski", "ova", "wed", "non", "wac", "nut", "why", "bye", "lye", "oct", "old", "fin", "feb",
+        "chi", "sap", "owl", "log", "tod", "dot", "bow", "fob", "for", "joe", "ivy", "fan", "age",
+        "fax", "hip", "jib", "mel", "hus", "sob", "ifs", "tab", "ara", "dab", "jag", "jar", "arm",
+        "lot", "tom", "sax", "tex", "yum", "pei", "wen", "wry", "ire", "irk", "far", "mew", "wit",
+        "doe", "gas", "rte", "ian", "pot", "ask", "wag", "hag", "amy", "nag", "ron", "soy", "gin",
+        "don", "tug", "fay", "vic", "boo", "nam", "ave", "buy", "sop", "but", "orb", "fen", "paw",
+        "his", "sub", "bob", "yea", "oft", "inn", "rod", "yam", "pew", "web", "hod", "hun", "gyp",
+        "wei", "wis", "rob", "gad", "pie", "mon", "dog", "bib", "rub", "ere", "dig", "era", "cat",
+        "fox", "bee", "mod", "day", "apr", "vie", "nev", "jam", "pam", "new", "aye", "ani", "and",
+        "ibm", "yap", "can", "pyx", "tar", "kin", "fog", "hum", "pip", "cup", "dye", "lyx", "jog",
+        "nun", "par", "wan", "fey", "bus", "oak", "bad", "ats", "set", "qom", "vat", "eat", "pus",
+        "rev", "axe", "ion", "six", "ila", "lao", "mom", "mas", "pro", "few", "opt", "poe", "art",
+        "ash", "oar", "cap", "lop", "may", "shy", "rid", "bat", "sum", "rim", "fee", "bmw", "sky",
+        "maj", "hue", "thy", "ava", "rap", "den", "fla", "auk", "cox", "ibo", "hey", "saw", "vim",
+        "sec", "ltd", "you", "its", "tat", "dew", "eva", "tog", "ram", "let", "see", "zit", "maw",
+        "nix", "ate", "gig", "rep", "owe", "ind", "hog", "eve", "sam", "zoo", "any", "dow", "cod",
+        "bed", "vet", "ham", "sis", "hex", "via", "fir", "nod", "mao", "aug", "mum", "hoe", "bah",
+        "hal", "keg", "hew", "zed", "tow", "gog", "ass", "dem", "who", "bet", "gos", "son", "ear",
+        "spy", "kit", "boy", "due", "sen", "oaf", "mix", "hep", "fur", "ada", "bin", "nil", "mia",
+        "ewe", "hit", "fix", "sad", "rib", "eye", "hop", "haw", "wax", "mid", "tad", "ken", "wad",
+        "rye", "pap", "bog", "gut", "ito", "woe", "our", "ado", "sin", "mad", "ray", "hon", "roy",
+        "dip", "hen", "iva", "lug", "asp", "hui", "yak", "bay", "poi", "yep", "bun", "try", "lad",
+        "elm", "nat", "wyo", "gym", "dug", "toe", "dee", "wig", "sly", "rip", "geo", "cog", "pas",
+        "zen", "odd", "nan", "lay", "pod", "fit", "hem", "joy", "bum", "rio", "yon", "dec", "leg",
+        "put", "sue", "dim", "pet", "yaw", "nub", "bit", "bur", "sid", "sun", "oil", "red", "doc",
+        "moe", "caw", "eel", "dix", "cub", "end", "gem", "off", "yew", "hug", "pop", "tub", "sgt",
+        "lid", "pun", "ton", "sol", "din", "yup", "jab", "pea", "bug", "gag", "mil", "jig", "hub",
+        "low", "did", "tin", "get", "gte", "sox", "lei", "mig", "fig", "lon", "use", "ban", "flo",
+        "nov", "jut", "bag", "mir", "sty", "lap", "two", "ins", "con", "ant", "net", "tux", "ode",
+        "stu", "mug", "cad", "nap", "gun", "fop", "tot", "sow", "sal", "sic", "ted", "wot", "del",
+        "imp", "cob", "way", "ann", "tan", "mci", "job", "wet", "ism", "err", "him", "all", "pad",
+        "hah", "hie", "aim", "ike", "jed", "ego", "mac", "baa", "min", "com", "ill", "was", "cab",
+        "ago", "ina", "big", "ilk", "gal", "tap", "duh", "ola", "ran", "lab", "top", "gob", "hot",
+        "ora", "tia", "kip", "han", "met", "hut", "she", "sac", "fed", "goo", "tee", "ell", "not",
+        "act", "gil", "rut", "ala", "ape", "rig", "cid", "god", "duo", "lin", "aid", "gel", "awl",
+        "lag", "elf", "liz", "ref", "aha", "fib", "oho", "tho", "her", "nor", "ace", "adz", "fun",
+        "ned", "coo", "win", "tao", "coy", "van", "man", "pit", "guy", "foe", "hid", "mai", "sup",
+        "jay", "hob", "mow", "jot", "are", "pol", "arc", "lax", "aft", "alb", "len", "air", "pug",
+        "pox", "vow", "got", "meg", "zoe", "amp", "ale", "bud", "gee", "pin", "dun", "pat", "ten",
+        "mob"
+      ])
+    )
+  )
 }
