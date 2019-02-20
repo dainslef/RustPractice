54ebc1e49b16495c068b5d2ed124a8c41af22fcf
dainslef <dainslef@outlook.com>
2019-02-20 14:37:51 +0800

    Use attribute "#[allow(dead_code)]" to avoid some unnecessary unused code warnings.

--- src/leetcode.rs
+++ src/leetcode.rs
@@ -5,29 +5,17 @@
  * warnning message like "warning: function is never used: `...`".
  */
 
-#[cfg(test)]
 mod q2_add_two_numbers;
-#[cfg(test)]
 mod q3_length_of_longest_substring;
-#[cfg(test)]
 mod q4_find_median_sorted_arrays;
-#[cfg(test)]
 mod q5_longest_palindrome;
-#[cfg(test)]
 mod q7_reverse_integer;
-#[cfg(test)]
 mod q8_my_atoi;
-#[cfg(test)]
 mod q15_three_sum;
-#[cfg(test)]
 mod q16_three_sum_closest;
-#[cfg(test)]
 mod q18_four_sum;
-#[cfg(test)]
 mod q126_find_ladders;
-#[cfg(test)]
 mod q445_add_two_numbers_two;
-#[cfg(test)]
 mod q454_four_sum_two;
 
 #[derive(PartialEq, Eq, Debug)]
