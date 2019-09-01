c2793b7df6738da13e6ecd77a834d33af469b385
dainslef <dainslef@outlook.com>
2019-09-01 23:49:10 +0800

    Add some code comments for the "First Missing Positive" problem.

--- src/leetcode/q41_first_missing_positive.rs
+++ src/leetcode/q41_first_missing_positive.rs
@@ -18,12 +18,17 @@
  * Your algorithm should run in O(n) time and uses constant extra space.
  */
 
-// "constant extra space" means the soultion shouldn't use container from std library like Vec/Set/List and so on which dynamic allocate memory
+// "constant extra space" means the soultion shouldn't use container from std library,
+// like Vec/Set/List and so on which dynamic allocate memory
 fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
-  // make the index as the number indicator, like "nums[0] = 1, nums[1] = 2, ..."
+  // make the index as the number indicator (nums[i] = i + 1 or nums[i - 1] = i)
+  // for example "nums[0] = 1, nums[1] = 2, ..."
   for i in 0..nums.len() {
     let mut v = nums[i];
+    // check if the current number is valid (in range 1..=num.len()),
+    // and match the rule (v = nums[v - 1])
     while v > 0 && v <= nums.len() as i32 && v != nums[v as usize - 1] {
+      // if the current number is valid but doesn't match the rule, place the number on correct index
       let temp = nums[v as usize - 1];
       nums[v as usize - 1] = v;
       nums[i] = temp;
@@ -31,13 +36,16 @@ fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
     }
   }
 
+  // traverse elements, find the first value doesn't match "num[i] = i + 1"
   for i in 0..nums.len() {
     let v = i as i32 + 1;
     if nums[i] != v {
+      // if the element doesn't match the rule, it's the first missing positive integer
       return v;
     }
   }
 
+  // if all the elements match the rule, the first missing positive integer is the next integer
   nums.len() as i32 + 1
 }
 
