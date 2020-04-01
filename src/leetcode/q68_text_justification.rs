d3142d0a9a921d778b48c837ea981f563a77e18c
dainslef <dainslef@outlook.com>
2020-04-02 00:30:50 +0800

    Refactoring code and add some comments for the problem "Text Justification".

--- src/leetcode/q68_text_justification.rs
+++ src/leetcode/q68_text_justification.rs
@@ -58,42 +58,54 @@
  */
 
 fn full_justify(words: Vec<String>, max_width: i32) -> Vec<String> {
-  let (mut out, mut temp): (Vec<String>, Vec<String>) = (Vec::new(), Vec::new());
+  let (mut out, mut temp_words): (Vec<String>, Vec<String>) = (Vec::new(), Vec::new());
   let max_width = max_width as usize;
 
   for word in words {
-    let word_size: usize = temp.iter().map(|v| v.len()).sum();
-    if word_size + word.len() + temp.len() > max_width {
-      let size = if temp.len() > 2 { temp.len() - 1 } else { 1 };
-      let space_size = max_width - word_size;
-      let (space_width, extra) = (space_size / size, space_size % size);
-      let mut temp_s = "".to_string();
+    let word_size: usize = temp_words.iter().map(|v| v.len()).sum();
+    let width = word_size + word.len() + temp_words.len(); // compute the all text width, at least need the size of all the words plus the word count size minus one (for word space)
+
+    if width > max_width {
+      // compute the number of space block, space block should be "word count minus one", and as least one need space block
+      let size = if temp_words.len() > 2 {
+        temp_words.len() - 1
+      } else {
+        1
+      };
+
+      let space_size = max_width - word_size; // compute the size of space
+      let (space_width, extra) = (space_size / size, space_size % size); // compute the each size of space, and the position of extra size (make the space distributed as evenly as possible)
+
+      let mut s = "".to_string();
       for i in 0..size {
-        temp_s += &temp[i];
-        temp_s += &(0..space_width).map(|_| ' ').collect::<String>();
+        s += &temp_words[i]; // add the word
+        s += &(0..space_width).map(|_| ' ').collect::<String>(); // add the normal space
         if i < extra {
-          temp_s += &" ";
+          s += &" "; // check if need to add the extra space
         }
       }
-      if temp.len() > 1 {
-        temp_s += temp.last().unwrap();
+
+      if temp_words.len() > 1 {
+        s += temp_words.last().unwrap();
       }
-      out.push(temp_s);
-      temp.clear();
+
+      out.push(s);
+      temp_words.clear();
     }
-    temp.push(word);
+    temp_words.push(word);
   }
 
-  if !temp.is_empty() {
+  // add the remaining words
+  if !temp_words.is_empty() {
     let mut s = "".to_string();
-    for word in temp {
+    for word in temp_words {
       if !s.is_empty() {
-        s.push(' ');
+        s.push(' '); // check if the content already has chars, add space between two words
       }
       s += &word;
     }
     for _ in s.len()..max_width {
-      s.push(' ');
+      s.push(' '); // fill with space to match the max width
     }
     out.push(s);
   }
@@ -103,10 +115,7 @@ fn full_justify(words: Vec<String>, max_width: i32) -> Vec<String> {
 
 #[test]
 fn q68_test() {
-  assert_eq!(
-    full_justify(string_vec!["test"], 4),
-    ["test"]
-  );
+  assert_eq!(full_justify(string_vec!["test"], 4), ["test"]);
   assert_eq!(
     full_justify(
       string_vec!["What", "must", "be", "acknowledgment", "shall", "be"],
