7f4bc929f1f584eaabcead32fdc9d0ab943ce2e0
dainslef <dainslef@outlook.com>
2020-05-09 23:32:17 +0800

    Add a new solution for the problem "#844 Backspace String Compare".

--- src/leetcode/q844_backspace_string_compare.rs
+++ src/leetcode/q844_backspace_string_compare.rs
@@ -35,7 +35,7 @@
  */
 
 // O(1) space means you can't create another new data structure
-fn backspace_compare(s: String, t: String) -> bool {
+fn backspace_compare_recursion(s: String, t: String) -> bool {
   fn recurse(mut s: Vec<u8>, mut t: Vec<u8>) -> bool {
     let (sharp, sharp_ref) = (Some('#' as u8), Some(&('#' as u8)));
 
@@ -70,12 +70,52 @@ fn backspace_compare(s: String, t: String) -> bool {
   recurse(s.into_bytes(), t.into_bytes())
 }
 
+// loop solution (need confirm !!!)
+fn backspace_compare(s: String, t: String) -> bool {
+  let (mut s, mut t) = (s.into_bytes(), t.into_bytes());
+  let (sharp, sharp_ref) = (Some('#' as u8), Some(&('#' as u8)));
+
+  loop {
+    macro_rules! remove_sharp {
+      ($bytes: expr) => {
+        if $bytes.last() == sharp_ref {
+          $bytes.pop();
+          let mut count = 1;
+          while count > 0 {
+            if $bytes.pop() == sharp {
+              count += 1;
+            } else {
+              count -= 1;
+            }
+          }
+        }
+      };
+    }
+
+    remove_sharp!(s);
+    remove_sharp!(t);
+
+    if s.last() != sharp_ref && t.last() != sharp_ref {
+      if s.is_empty() || t.is_empty() {
+        break s == t;
+      } else if s.pop() != t.pop() {
+        break false;
+      }
+    }
+  }
+}
+
 #[test]
 fn q844_test() {
-  assert_eq!(backspace_compare("a##c".into(), "#a#c".into()), true);
-  assert_eq!(backspace_compare("ab##".into(), "c#d#".into()), true);
-  assert_eq!(
-    backspace_compare("xywrrmp".into(), "xywrrmu#p".into()),
-    true
-  );
+  fn test(backspace_compare: impl Fn(String, String) -> bool) {
+    assert_eq!(backspace_compare("a##c".into(), "#a#c".into()), true);
+    assert_eq!(backspace_compare("ab##".into(), "c#d#".into()), true);
+    assert_eq!(
+      backspace_compare("xywrrmp".into(), "xywrrmu#p".into()),
+      true
+    );
+  }
+
+  test(backspace_compare);
+  test(backspace_compare_recursion);
 }
