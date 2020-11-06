61bd65d22ea8f945d47976e20c9842437e396345
dainslef <dainslef@outlook.com>
2020-11-06 23:51:57 +0800

    Refactoring the code for the problem "#82 Remove Duplicates from Sorted List II".

--- src/leetcode/q82_remove_duplicates_from_sorted_list_ii.rs
+++ src/leetcode/q82_remove_duplicates_from_sorted_list_ii.rs
@@ -25,7 +25,6 @@ fn delete_duplicates_by_value(head: Option<Box<ListNode>>) -> Option<Box<ListNod
   use std::collections::HashSet;
 
   let (mut nums, mut next, mut same) = (vec![], head, HashSet::new());
-
   while let Some(v) = next {
     if nums.last().map(|l| *l == v.val).unwrap_or(false) {
       same.insert(v.val); // record the same elements
@@ -51,33 +50,26 @@ fn delete_duplicates_by_value(head: Option<Box<ListNode>>) -> Option<Box<ListNod
  * Memory Usage: 2 MB, less than 71.43% of Rust online submissions for Remove Duplicates from Sorted List II.
  */
 fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-  // use Option::take() to take the value out of the Option, and then leaving a None in its place.
-
-  let mut last = &mut None as *mut Option<Box<ListNode>>;
-  let mut current = &mut head as *mut Option<Box<ListNode>>;
+  let mut last: *mut Option<Box<ListNode>> = &mut None;
+  let mut current: *mut Option<Box<ListNode>> = &mut head;
   let mut same: Option<i32> = None;
 
   unsafe {
     while let Some(c) = &mut *current {
+      macro_rules! exchange {
+        ($current: expr, $empty_same: expr) => {{
+          if $empty_same {
+            same = Some(c.val);
+          }
+          $current = c.next.take();
+          current = &mut $current;
+        }};
+      }
       match (&mut *last, &mut c.next, same) {
-        (Some(l), _, Some(s)) if c.val == s => {
-          l.next = c.next.take();
-          current = &mut l.next;
-        }
-        (None, _, Some(s)) if c.val == s => {
-          head = c.next.take();
-          current = &mut head;
-        }
-        (Some(l), Some(n), _) if c.val == n.val => {
-          same = Some(c.val);
-          l.next = n.next.take();
-          current = &mut l.next;
-        }
-        (None, Some(n), _) if c.val == n.val => {
-          same = Some(c.val);
-          head = n.next.take();
-          current = &mut head;
-        }
+        (Some(l), _, Some(s)) if c.val == s => exchange!(l.next, false),
+        (None, _, Some(s)) if c.val == s => exchange!(head, false),
+        (Some(l), Some(n), _) if c.val == n.val => exchange!(l.next, true),
+        (None, Some(n), _) if c.val == n.val => exchange!(head, true),
         _ => {
           last = current;
           current = &mut c.next;
