0ad8cf6360cefbc94368cb267d02ca9c91cd0001
dainslef <dainslef@outlook.com>
2020-11-17 14:04:05 +0800

    Refactoring the code for the problem "#87 Scramble String".

--- src/leetcode/q87_scramble_string.rs
+++ src/leetcode/q87_scramble_string.rs
@@ -48,60 +48,47 @@
  * Memory Usage: 2.1 MB
  */
 fn is_scramble(s1: String, s2: String) -> bool {
-  use std::collections::HashMap;
-
-  if s1 == s2 {
-    return true;
-  }
-  if s1.len() == 1 || s1.len() != s2.len() {
-    return false;
-  }
-
-  let (mut chars_map1, mut chars_map2) = (HashMap::new(), HashMap::new());
-  for i in 0..s1.len() {
-    if let (Some(k1), Some(k2)) = (s1.get(i..i + 1), s2.get(i..i + 1)) {
-      *chars_map1.entry(k1).or_insert(0) += 1;
-      *chars_map2.entry(k2).or_insert(0) += 1;
+    if s1 == s2 {
+        return true;
     }
-  }
-  if chars_map1 != chars_map2 {
-    return false;
-  }
-
-  macro_rules! check {
-    ($l1: expr, $r1: expr, $l2: expr, $r2: expr) => {{
-      let mut result = false;
-      if $l1.len() == $r1.len() {
-        result |= is_scramble($l1.into(), $r1.into()) && is_scramble($l2.into(), $r2.into())
-      }
-      if !result && $l1.len() == $r2.len() {
-        result |= is_scramble($l1.into(), $r2.into()) && is_scramble($l2.into(), $r1.into())
-      }
-      result
-    }};
-  }
-
-  for i in 1..s1.len() {
-    let (s1_head, s1_tail) = s1.split_at(i);
-    let (s2_head1, s2_tail1) = s2.split_at(i);
-    let (s2_head2, s2_tail2) = s2.split_at(s1.len() - i);
-    if check!(s1_head, s2_head1, s1_tail, s2_tail1) || check!(s1_head, s2_head2, s1_tail, s2_tail2)
-    {
-      return true;
+    if s1.len() == 1 || s1.len() != s2.len() {
+        return false;
     }
-  }
 
-  false
+    let (mut chars_count1, mut chars_count2) = ([0; 26], [0; 26]);
+    let (ascii, size) = ('a' as usize, s1.len());
+    for i in 0..size {
+        chars_count1[s1.as_bytes()[i] as usize - ascii] += 1;
+        chars_count2[s2.as_bytes()[i] as usize - ascii] += 1;
+    }
+    if chars_count1 != chars_count2 {
+        return false; // if s1 and s2 have different size of chars, must not be scramble
+    }
+
+    for i in 1..size {
+        let (s1_head, s1_tail) = s1.split_at(i);
+        let (s2_head1, s2_tail1) = s2.split_at(i);
+        let (s2_tail2, s2_head2) = s2.split_at(size - i);
+        if is_scramble(s1_head.into(), s2_head1.into())
+            && is_scramble(s1_tail.into(), s2_tail1.into())
+            || is_scramble(s1_head.into(), s2_head2.into())
+                && is_scramble(s1_tail.into(), s2_tail2.into())
+        {
+            return true;
+        }
+    }
+
+    false
 }
 
 #[test]
 fn q87_test() {
-  assert!(!is_scramble(
-    "abcdefghijklmnopq".into(),
-    "efghijklmnopqcadb".into()
-  ));
-  assert!(is_scramble("abb".into(), "bba".into()));
-  assert!(is_scramble("great".into(), "rgeat".into()));
-  assert!(!is_scramble("abcde".into(), "caebd".into()));
-  assert!(is_scramble("a".into(), "a".into()));
+    assert!(!is_scramble(
+        "abcdefghijklmnopq".into(),
+        "efghijklmnopqcadb".into()
+    ));
+    assert!(is_scramble("abb".into(), "bba".into()));
+    assert!(is_scramble("great".into(), "rgeat".into()));
+    assert!(!is_scramble("abcde".into(), "caebd".into()));
+    assert!(is_scramble("a".into(), "a".into()));
 }
