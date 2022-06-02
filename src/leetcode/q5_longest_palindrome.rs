5c98317d5272b92c52ea156c7b12cfc452e1def3
dainslef <dainslef@outlook.com>
2022-06-02 23:26:39 +0800

    Update some problems' descriptions.

--- src/leetcode/q5_longest_palindrome.rs
+++ src/leetcode/q5_longest_palindrome.rs
@@ -1,16 +1,23 @@
-/**
- * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
- *
- * Example 1:
- *
- * Input: "babad"
- * Output: "bab"
- * Note: "aba" is also a valid answer.
- * Example 2:
- *
- * Input: "cbbd"
- * Output: "bb"
- */
+/*!
+[5. Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/)
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+```html
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+```
+
+Example 2:
+
+```html
+Input: "cbbd"
+Output: "bb"
+```
+*/
 
 fn longest_palindrome_back(s: String) -> String {
   use std::collections::HashMap;
--- src/leetcode/q7_reverse_integer.rs
+++ src/leetcode/q7_reverse_integer.rs
@@ -1,21 +1,33 @@
-/**
- * Given a 32-bit signed integer, reverse digits of an integer.
- *
- * Example 1:
- *
- * Input: 123
- * Output: 321
- * Example 2:
- *
- * Input: -123
- * Output: -321
- * Example 3:
- *
- * Input: 120
- * Output: 21
- * Note:
- * Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
- */
+/*!
+[7. Reverse Integer](https://leetcode.com/problems/reverse-integer/)
+
+Given a signed 32-bit integer `x`, return `x` with its digits reversed.
+If reversing `x` causes the value to go outside the signed 32-bit integer range `[-2^31, 2^31 - 1]`, then return 0.
+
+Example 1:
+
+```html
+Input: 123
+Output: 321
+```
+
+Example 2:
+
+```html
+Input: -123
+Output: -321
+```
+
+Example 3:
+
+```html
+Input: 120
+Output: 21
+```
+
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+*/
 
 fn reverse(x: i32) -> i32 {
   let mut new_str = String::new();
