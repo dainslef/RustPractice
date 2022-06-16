9e100fcb3e2412b3536de50ae7a59506859e3dc4
dainslef <dainslef@outlook.com>
2022-06-17 04:20:04 +0800

    Add a new solution for the problem "#94 Binary Tree Inorder Traversal".

--- src/leetcode/q94_binary_tree_inorder_traversal.rs
+++ src/leetcode/q94_binary_tree_inorder_traversal.rs
@@ -1,52 +1,64 @@
-/**
- * 94. Binary Tree Inorder Traversal
- * https://leetcode.com/problems/binary-tree-inorder-traversal/
- *
- * Given the root of a binary tree, return the inorder traversal of its nodes' values.
- *
- *
- *
- * Example 1:
- *
- *
- * Input: root = [1,null,2,3]
- * Output: [1,3,2]
- * Example 2:
- *
- * Input: root = []
- * Output: []
- * Example 3:
- *
- * Input: root = [1]
- * Output: [1]
- * Example 4:
- *
- *
- * Input: root = [1,2]
- * Output: [2,1]
- * Example 5:
- *
- *
- * Input: root = [1,null,2]
- * Output: [1,2]
- *
- *
- * Constraints:
- *
- * The number of nodes in the tree is in the range [0, 100].
- * -100 <= Node.val <= 100
- */
+/*!
+[94. Binary Tree Inorder Traversal](https://leetcode.com/problems/binary-tree-inorder-traversal/)
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+
+```
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+```
+
+Example 2:
+
+```
+Input: root = []
+Output: []
+```
+
+Example 3:
+
+```
+Input: root = [1]
+Output: [1]
+```
+
+Example 4:
+
+```
+Input: root = [1,2]
+Output: [2,1]
+```
+
+Example 5:
+
+```
+Input: root = [1,null,2]
+Output: [1,2]
+```
+
+Constraints:
+
+- The number of nodes in the tree is in the range [0, 100].
+- -100 <= Node.val <= 100
+*/
 use super::*;
 
 /**
- * Runtime: 0 ms, faster than 100.00% of Rust online submissions for Binary Tree Inorder Traversal.
- * Memory Usage: 1.9 MB, less than 93.55% of Rust online submissions for Binary Tree Inorder Traversal.
- *
- * Depth First Traversals:
- * Inorder (Left, Root, Right)
- * Preorder (Root, Left, Right)
- * Postorder (Left, Right, Root)
- */
+Runtime: 0 ms, faster than 100.00% of Rust online submissions for Binary Tree Inorder Traversal.<br>
+Memory Usage: 1.9 MB, less than 93.55% of Rust online submissions for Binary Tree Inorder Traversal.
+
+Solution based on recursion.
+
+Depth First Traversals:
+
+```
+Inorder (Left, Root, Right)
+Preorder (Root, Left, Right)
+Postorder (Left, Right, Root)
+```
+*/
 fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
   let mut temp = vec![];
 
@@ -62,20 +74,56 @@ fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
   temp
 }
 
+/**
+Runtime: 1 ms, faster than 73.04% of Rust online submissions for Binary Tree Inorder Traversal.<br>
+Memory Usage: 2.2 MB, less than 33.91% of Rust online submissions for Binary Tree Inorder Traversal.
+
+Loop-based solution.
+*/
+fn inorder_traversal_loop(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+  let (mut temp, mut nodes) = (vec![], vec![root]);
+
+  while let Some(Some(v)) = nodes.last_mut() {
+    // it must use unafe to get the raw pointer here.
+    // if you use "borrow_mut()" to get the safe mut pointer here,
+    // it will against the onwership rules. ("v" use the mutable reference of "nodes" in line 74,
+    // so "nodes" can't be used for other mutable operate, like "nodes.push(xxx)" in line 85)
+    let left = unsafe { &mut (*v.as_ptr()).left };
+    if left.is_some() {
+      nodes.push(left.take()); // if the left node is exist, access all left node first
+    } else if let Some(Some(n)) = nodes.pop() {
+      let mut n = n.borrow_mut();
+      temp.push(n.val); // if the left node is not exist, then push the value
+      if n.right.is_some() {
+        // if current node has right child, set the node as a new access point,
+        // and continue to access the next left node
+        nodes.push(n.right.take());
+      }
+    }
+  }
+
+  temp
+}
+
 #[test]
 fn q94_test() {
-  assert_eq!(
-    inorder_traversal(TreeNode::from(vec![Some(1), None, Some(2), Some(3)])),
-    [1, 3, 2]
-  );
-  assert_eq!(inorder_traversal(TreeNode::from(vec![])), []);
-  assert_eq!(inorder_traversal(TreeNode::from(vec![Some(1)])), [1]);
-  assert_eq!(
-    inorder_traversal(TreeNode::from(vec![Some(1), Some(2)])),
-    [2, 1]
-  );
-  assert_eq!(
-    inorder_traversal(TreeNode::from(vec![Some(1), None, Some(2)])),
-    [1, 2]
-  );
+  fn test(inorder_traversal: impl Fn(Option<Rc<RefCell<TreeNode>>>) -> Vec<i32>) {
+    assert_eq!(
+      inorder_traversal(TreeNode::from(vec![Some(1), None, Some(2), Some(3)])),
+      [1, 3, 2]
+    );
+    assert_eq!(inorder_traversal(TreeNode::from(vec![])), []);
+    assert_eq!(inorder_traversal(TreeNode::from(vec![Some(1)])), [1]);
+    assert_eq!(
+      inorder_traversal(TreeNode::from(vec![Some(1), Some(2)])),
+      [2, 1]
+    );
+    assert_eq!(
+      inorder_traversal(TreeNode::from(vec![Some(1), None, Some(2)])),
+      [1, 2]
+    );
+  }
+
+  test(inorder_traversal);
+  test(inorder_traversal_loop);
 }
