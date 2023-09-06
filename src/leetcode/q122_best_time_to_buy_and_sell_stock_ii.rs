4eb394fcc7ba7d35d4954d2b53a212d0853f80ce
dainslef <dainslef@outlook.com>
2023-09-06 11:32:21 +0800

    Update the doc to fix the CI error.

--- src/leetcode/q122_best_time_to_buy_and_sell_stock_ii.rs
+++ src/leetcode/q122_best_time_to_buy_and_sell_stock_ii.rs
@@ -1,31 +1,44 @@
-/**
- * 122. Best Time to Buy and Sell Stock II
- *
- * Say you have an array for which the ith element is the price of a given stock on day i.
- *
- * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
- *
- * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
- *
- * Example 1:
- *
- * Input: [7,1,5,3,6,4]
- * Output: 7
- * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
- *              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
- * Example 2:
- *
- * Input: [1,2,3,4,5]
- * Output: 4
- * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
- *              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
- *              engaging multiple transactions at the same time. You must sell before buying again.
- * Example 3:
- *
- * Input: [7,6,4,3,1]
- * Output: 0
-Explanation: In this case, no transaction is done, i.e. max profit = 0.
- */
+/*!
+[122. Best Time to Buy and Sell Stock II](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/)
+
+You are given an integer array `prices` where `prices[i]` is the price of a given stock on the `i^th` day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return *the maximum profit you can achieve*.
+
+Example 1:
+
+```html
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+```
+
+Example 2:
+
+```html
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+```
+
+Example 3:
+
+```html
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+```
+
+Constraints:
+
+- `1 <= prices.length <= 3 * 10^4`
+- `0 <= prices[i] <= 104`
+*/
 
 fn max_profit(prices: Vec<i32>) -> i32 {
   let mut sum = 0;
